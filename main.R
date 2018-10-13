d3b5c65c902b47a365aa3434cd585a189b9d75b4
vwmaus <vwmaus1@gmail.com>
2018-10-13 14:12:28 +0200

    Test viridis using discreate colours

--- main.R
+++ main.R
@@ -25,7 +25,16 @@ footprint_map <- world_map %>%
   dplyr::group_by(Region) %>% 
   dplyr::filter(!is.na(Region)) %>% 
   dplyr::summarise(water_nordic = mean(water_nordic, na.rm = TRUE),
-                   land_nordic = mean(land_nordic, na.rm = TRUE))
+                   land_nordic = mean(land_nordic, na.rm = TRUE)) %>% 
+  dplyr::mutate(water_classes = ifelse(water_nordic < 25, "0-25",
+                                       ifelse(water_nordic < 50, "25-50", 
+                                              ifelse(water_nordic < 75, "50-75",
+                                                     ifelse(water_nordic < 100, "75-100",
+                                                            ifelse(water_nordic < 125, "100-125",
+                                                                   ifelse(water_nordic < 150, "125-150",
+                                                                          ifelse(water_nordic < 175, "150-175",
+                                                                                 ifelse(water_nordic < 200, "175-200", ">200"))))))))) %>% 
+  dplyr::mutate(water_classes = factor(water_classes))
 
 # Save geodata 
 sf::write_sf(footprint_map, dsn = "./output/footprint_map.geojson", driver = "GeoJSON", delete_dsn = TRUE)
@@ -47,9 +56,9 @@ colors[[4]] <- c("#eff3ff", "#c6dbef", "#9ecae1", "#6baed6", "#3182bd", "#08519c
 colors[[5]] <- c("#c6dbef", "#9ecae1", "#6baed6", "#4292c6", "#2171b5", "#08519c", "#08306b")
 
 gp_map <- ggplot2::ggplot(footprint_map) +
-  ggplot2::geom_sf(aes(fill = water_nordic), size = 0.1, ) +
+  ggplot2::geom_sf(aes(fill = water_classes), size = 0.1) +
   ggthemes::theme_map() +
-  viridis::scale_fill_viridis(direction = -1, option = "viridis") +
+  viridis::scale_fill_viridis(option = "viridis", na.value = "#dadada", discrete = TRUE) +
   # ggplot2::scale_fill_gradientn(colors = colors[[5]]) + 
   ggplot2::geom_sf(data = footprint_map_centroid, aes(size = land_nordic), show.legend = 'point', fill = "#f39642", colour="white", shape = 21) +
   # scale_colour_manual(values = "#212121") + 
@@ -57,13 +66,13 @@ gp_map <- ggplot2::ggplot(footprint_map) +
   
 gp_map
  
-ggplot2::ggsave(paste0("viridis_global_footprint_map.pdf"), plot = gp_map, path = "./output",
+ggplot2::ggsave(paste0("viridis_discrete_global_footprint_map.pdf"), plot = gp_map, path = "./output",
                 scale = 1, width = 207, height = 90, units = "mm", dpi = 600)
 
-ggplot2::ggsave(paste0("viridis_global_footprint_map.eps"), plot = gp_map, path = "./output",
+ggplot2::ggsave(paste0("viridis_discrete_global_footprint_map.eps"), plot = gp_map, path = "./output",
                 scale = 1, width = 207, height = 90, units = "mm", dpi = 600)
 
-ggplot2::ggsave(paste0("viridis_global_footprint_map.png"), plot = gp_map, path = "./output",
+ggplot2::ggsave(paste0("viridis_discrete_global_footprint_map.png"), plot = gp_map, path = "./output",
                 scale = 1, width = 207, height = 90, units = "mm", dpi = 600)
 
 # Get zoom to EU 
@@ -78,24 +87,24 @@ eu_bbox[4] <- eu_bbox[4] + 0
 
 gp_map_eu <- footprint_map %>% 
   sf::st_crop(eu_bbox) %>% 
-  dplyr::select(water_nordic) %>% 
+  dplyr::select(water_classes) %>% 
   ggplot2::ggplot() +
-  ggplot2::geom_sf(aes(fill = water_nordic), size = 0.1) +
+  ggplot2::geom_sf(aes(fill = water_classes), size = 0.1) +
   ggthemes::theme_map() +
-  viridis::scale_fill_viridis(direction = -1, option = "viridis", na.value = "#dadada") +
+  viridis::scale_fill_viridis(option = "viridis", na.value = "#dadada", discrete = TRUE) +
   # ggplot2::scale_fill_gradientn(colors = colors[[5]]) + 
   ggplot2::geom_sf(data = sf::st_crop(footprint_map_centroid, eu_bbox), aes(size = land_nordic), show.legend = 'point', fill = "#f39642", colour="white", shape = 21) +
   ggplot2::theme(legend.position = "left", panel.grid.major = element_line(color = "white")) 
 
 gp_map_eu
 
-ggplot2::ggsave(paste0("viridis_global_footprint_map_eu.pdf"), plot = gp_map_eu, path = "./output",
+ggplot2::ggsave(paste0("viridis_discrete_global_footprint_map_eu.pdf"), plot = gp_map_eu, path = "./output",
                 scale = 1, width = 207, height = 90, units = "mm", dpi = 600)
 
-ggplot2::ggsave(paste0("viridis_global_footprint_map_eu.eps"), plot = gp_map_eu, path = "./output",
+ggplot2::ggsave(paste0("viridis_discrete_global_footprint_map_eu.eps"), plot = gp_map_eu, path = "./output",
                 scale = 1, width = 207, height = 90, units = "mm", dpi = 600)
 
-ggplot2::ggsave(paste0("viridis_global_footprint_map_eu.png"), plot = gp_map_eu, path = "./output",
+ggplot2::ggsave(paste0("viridis_discrete_global_footprint_map_eu.png"), plot = gp_map_eu, path = "./output",
                 scale = 1, width = 207, height = 90, units = "mm", dpi = 600)
 
 
